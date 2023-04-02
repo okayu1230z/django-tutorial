495cfc54f190f2e8dc97cb409dc18817f8921a6c
okmt1230z <okmt1230z@gmail.com>
2023-04-02 18:57:55 +0900

    create first model

--- app/mysite/polls/models.py
+++ app/mysite/polls/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200) # CharField は文字フィールド
+    pub_date = models.DateTimeField('date published') # DateTimeField は日時フィールド
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Choiceが1つのQuestionに関連づけられることを定義している
+    choice_text = models.CharField(max_length=200) # CharField にはmex_lengthが必須項目
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
